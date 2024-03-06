7e6d5552ab0f3dcf6303baf5bb0deef1e8ab3343
olan-tayo <olantayo3627@gmail.com>
2024-03-06 16:35:42 +0100

    feat: implement tab

--- src/tests/button.test.tsx
+++ src/tests/button.test.tsx
@@ -3,10 +3,30 @@ import { render } from '@testing-library/react'
 
 import 'jest-canvas-mock'
 
-import { Button } from '..'
+import { Button, Tab } from '..'
+
+const data = [
+  { key: 1, value: 'One' },
+  { key: 2, value: 'Two' },
+  { key: 3, value: 'Three' },
+]
+
+const handleSelectedTab = () => {}
 
 describe('Render Button', () => {
   it('renders button without crashing', () => {
     render(<Button>Test</Button>)
+  })
+})
+
+describe('Render Tab', () => {
+  it('renders tab without crashing', () => {
+    render(
+      <Tab tabText='' tabContainer='' onSelectedTab={handleSelectedTab} data={data}>
+        <div>Yes one</div>
+        <div>Yes two</div>
+        <div>Yes three</div>
+      </Tab>,
+    )
   })
 })
